3de4fa733cbfa015c1fa7288387cab467f38c5ff
lucianefalcao <lucianefalcaoff@gmail.com>
2020-05-23 06:12:06 -0300

    add images and model

---
+++ ui/lib/models/dress_model.dart
@@ -0,0 +1,78 @@
+class Dress {
+  final String image;
+  final String description;
+  final int price;
+  final int rating;
+  bool isMarked;
+
+  Dress({this.image, this.description, this.price, this.rating, this.isMarked});
+}
+
+List<Dress> popularList = [
+  Dress(
+    image: "assets/images/wedding_dress_1.jpg",
+    price: 400,
+    rating: 5,
+    isMarked: false,
+    description: "This grown features a shorter, manageable train to maintain volume and weightlessness to the overall design, and zips beneath fabric-covered buttons. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac turpis egestas integer eget aliquet. Tincidunt vitae semper quis lectus nulla. Sapien eget mi proin sed. Dictum non consectetur a erat nam at. Est sit amet facilisis magna etiam."
+  ),
+  Dress(
+    image: "assets/images/wedding_dress_2.jpg",
+    price: 300,
+    rating: 4,
+    isMarked: false,
+    description: "This grown features a shorter, manageable train to maintain volume and weightlessness to the overall design, and zips beneath fabric-covered buttons. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac turpis egestas integer eget aliquet. Tincidunt vitae semper quis lectus nulla. Sapien eget mi proin sed. Dictum non consectetur a erat nam at. Est sit amet facilisis magna etiam."
+  ),
+  Dress(
+    image: "assets/images/wedding_dress_3.jpg",
+    price: 200,
+    rating: 4,
+    isMarked: false,
+    description: "This grown features a shorter, manageable train to maintain volume and weightlessness to the overall design, and zips beneath fabric-covered buttons. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac turpis egestas integer eget aliquet. Tincidunt vitae semper quis lectus nulla. Sapien eget mi proin sed. Dictum non consectetur a erat nam at. Est sit amet facilisis magna etiam."
+  ),
+  Dress(
+    image: "assets/images/wedding_dress_4.jpg",
+    price: 200,
+    rating: 4,
+    isMarked: false,
+    description: "This grown features a shorter, manageable train to maintain volume and weightlessness to the overall design, and zips beneath fabric-covered buttons. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac turpis egestas integer eget aliquet. Tincidunt vitae semper quis lectus nulla. Sapien eget mi proin sed. Dictum non consectetur a erat nam at. Est sit amet facilisis magna etiam."
+  ),
+  Dress(
+    image: "assets/images/wedding_dress_5.jpg",
+    price: 500,
+    rating: 5,
+    isMarked: false,
+    description: "This grown features a shorter, manageable train to maintain volume and weightlessness to the overall design, and zips beneath fabric-covered buttons. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac turpis egestas integer eget aliquet. Tincidunt vitae semper quis lectus nulla. Sapien eget mi proin sed. Dictum non consectetur a erat nam at. Est sit amet facilisis magna etiam."
+  ),
+  Dress(
+    image: "assets/images/wedding_dress_6.jpg",
+    price: 200,
+    rating: 4,
+    isMarked: false,
+    description: "This grown features a shorter, manageable train to maintain volume and weightlessness to the overall design, and zips beneath fabric-covered buttons. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac turpis egestas integer eget aliquet. Tincidunt vitae semper quis lectus nulla. Sapien eget mi proin sed. Dictum non consectetur a erat nam at. Est sit amet facilisis magna etiam."
+  )
+];
+
+List<Dress> recommendList = [
+  Dress(
+    image: "assets/images/wedding_dress_7.jpeg",
+    price: 300,
+    rating: 4,
+    isMarked: false,
+    description: "This grown features a shorter, manageable train to maintain volume and weightlessness to the overall design, and zips beneath fabric-covered buttons. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac turpis egestas integer eget aliquet. Tincidunt vitae semper quis lectus nulla. Sapien eget mi proin sed. Dictum non consectetur a erat nam at. Est sit amet facilisis magna etiam."
+  ),
+  Dress(
+    image: "assets/images/wedding_dress_8.jpeg",
+    price: 300,
+    rating: 4,
+    isMarked: false,
+    description: "This grown features a shorter, manageable train to maintain volume and weightlessness to the overall design, and zips beneath fabric-covered buttons. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac turpis egestas integer eget aliquet. Tincidunt vitae semper quis lectus nulla. Sapien eget mi proin sed. Dictum non consectetur a erat nam at. Est sit amet facilisis magna etiam."
+  ),
+  Dress(
+    image: "assets/images/wedding_dress_9.jpg",
+    price: 200,
+    rating: 4,
+    isMarked: false,
+    description: "This grown features a shorter, manageable train to maintain volume and weightlessness to the overall design, and zips beneath fabric-covered buttons. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac turpis egestas integer eget aliquet. Tincidunt vitae semper quis lectus nulla. Sapien eget mi proin sed. Dictum non consectetur a erat nam at. Est sit amet facilisis magna etiam."
+  ),
+];
